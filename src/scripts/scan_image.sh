a57b7b200252692d6c8e0479ee1ae1188cad9700
Jonas Galvão Xavier <jonas.agx@gmail.com>
2021-11-19 00:02:43 -0800

    bash lint fixes
    Signed-off-by: Jonas Galvão Xavier <jonas.agx@gmail.com>

--- src/scripts/scan_image.sh
+++ src/scripts/scan_image.sh
@@ -7,20 +7,20 @@ function ScanImage() {
         failOnSeverityFlag="-f ${FAIL_ON}"
     fi
 
-    if [ -z $IMAGE_NAME ]; then
+    if [ -z "$IMAGE_NAME" ]; then
         echo "IMAGE_NAME must be set"
         exit 1
     fi
 
-    if [ -z $OUTPUT_FORMAT ]; then
+    if [ -z "$OUTPUT_FORMAT" ]; then
         echo "OUTPUT_FORMAT must be set"
         exit 1
     fi
 
 
     curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sh -s -- -b . latest
-    TRIMMED_IMAGE_NAME=$(echo $IMAGE_NAME | tr -s '/.:' '-')
-    ./grype $IMAGE_NAME -o $OUTPUT_FORMAT > "${TRIMMED_IMAGE_NAME}-vuln.json" ${failOnSeverityFlag:+$failOnSeverityFlag}
+    TRIMMED_IMAGE_NAME=$(echo "$IMAGE_NAME" | tr -s '/.:' '-')
+    ./grype "$IMAGE_NAME" -o "$OUTPUT_FORMAT" > "${TRIMMED_IMAGE_NAME}-vuln.json" ${failOnSeverityFlag:+$failOnSeverityFlag}
 }
 
 # Will not run if sourced from another script.
